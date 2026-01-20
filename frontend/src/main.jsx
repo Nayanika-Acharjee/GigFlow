77fb18b51d98723d5f0a784035469104bcc67870
Nayanika-Acharjee <nayanikaacharjee@gmail.com>
2026-01-20 23:50:52 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import axios from "axios";
-
+axios.defaults.baseURL = "https://gigflow-backend-5rho.onrender.com";
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")).render(

81bcbdc300e9d84d67b6dcbd598722e5b96ee63f
Nayanika-Acharjee <nayanikaacharjee@gmail.com>
2026-01-20 16:52:56 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: import.meta.env.local.VITE_API_URL || "http://localhost:5000/api",
+ baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   withCredentials: true, //  for JWT cookies
   headers: {
     "Content-Type": "application/json",

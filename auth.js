432b7d3376ebdad96b09a06b41aec18eb0dbbfde
Nayanika-Acharjee <nayanikaacharjee@gmail.com>
2026-01-20 19:42:17 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -8,12 +8,12 @@ const generateToken = (res, userId) => {
     expiresIn: "7d",
   });
 
- res.cookie("token", token, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-});
+res.cookie("token", token, {
+    httpOnly: true,
+    secure: false, // true in production
+    sameSite: "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
 };
 

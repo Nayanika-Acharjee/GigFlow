baa36f0b3123082febf4047a2e6d7dd27417e6d7
Nayanika-Acharjee <nayanikaacharjee@gmail.com>
2026-01-20 14:26:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,60 @@
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config({ path: ".env.local" });
+
+
+import authRoutes from "./auth.routes.js";
+import gigRoutes from "./gig.routes.js";
+import bidRoutes from "./bid.routes.js";
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: [
+    "http://localhost:5173",
+    "https://gig-flow-lac.vercel.app/"
+  ],
+  credentials: true
+  })
+);
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/gigs", gigRoutes);
+app.use("/api/bids", bidRoutes);
+
+app.get("/", (req, res) => {
+  res.send("GigFlow API running");
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+if (!process.env.MONGO_URI) {
+  console.error("❌ MONGO_URI is missing in .env.local");
+  process.exit(1);
+}
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection failed:", err.message);
+    process.exit(1);
+  });

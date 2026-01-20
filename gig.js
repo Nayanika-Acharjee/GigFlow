cebab0d3af5e4e2a5716a2ed4e07991040c68238
Nayanika-Acharjee <nayanikaacharjee@gmail.com>
2026-01-21 00:16:22 +0530

    Update gig.js

--- gig.js
+++ gig.js
@@ -1,13 +1,32 @@
 import mongoose from "mongoose";
 
-export default mongoose.model(
-"Gig",
-new mongoose.Schema({
-title : String,
-describtion : String,
-budget : Number,
-ownerId:{type: mongoose.Schema.Types.ObjectId, ref:"User"},
-status: { type: String, enum: ["open", "assigned"], default: "open" }
-  }, { timestamps: true })
-   
+const gigSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    budget: {
+      type: Number,
+      required: true,
+    },
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["open", "assigned"],
+      default: "open",
+    },
+  },
+  { timestamps: true }
 );
+
+export default mongoose.model("Gig", gigSchema);
+

ba4cf3a370778c8c19ae388d2d33b6a5f3e15660
Nayanika-Acharjee <nayanikaacharjee@gmail.com>
2026-01-20 17:33:42 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -8,12 +8,13 @@ const generateToken = (res, userId) => {
     expiresIn: "7d",
   });
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: true, // true in production
-    sameSite: "none",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-  });
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
+
 };
 
 /* ---------------- REGISTER ---------------- */
